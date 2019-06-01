c81b06cc416788c2fd6c7d50e3bc975bd64bc22a
Yokotachen <yokota_chen@aliyun.com>
2019-06-01 18:07:02 +0800

    add logo title wxlogin

---
+++ src/pages/login/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="title">
+      <img class="logo" src="/static/images/icardlogo.png" />
+      <div>心愿便利贴</div>
+      <button style="margin-top:200px;" open-type="getUserInfo">使用微信登陆</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted () {
+    wx.login({
+      success (res) {
+        if (res.code) {
+          console.log(res.code)
+        }
+      }
+    })
+  }
+}
+</script>
+
+<style scoped>
+.title {
+  text-align: center;
+}
+
+.logo {
+  width: 80px;
+  height: 60px;
+  display: inline-block;
+  overflow: hidden;
+}
+</style>
