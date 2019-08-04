417d7641579c80dd1596697a33591d92bfc162b5
shaopigan <243107208@qq.com>
2019-08-04 11:11:47 +0800

    2

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
     <div class="title">
-      <!--<img class="logo" src="/static/images/icardlogo.png">-->
-      <i-modal title="微信授权" :visible="visible2" @ok="handleOk" @cancel="handleCancel">
+      <img class="logo" src="/static/images/icardlogo.png">
+      <i-button style="margin-top:200px;" open-type="getUserInfo">使用微信登陆</i-button>
+      <!--<i-modal title="微信授权" :visible="visible2" @ok="handleOk" @cancel="handleCancel">
         <view class="headtext">心愿便利贴申请获得以下权限：</view>
         <view>
           <input type="checkbox" disabled checked>
           <span style="font-size: 0.7rem">获得您的公开信息(昵称、头像等)</span>
         </view>
-      </i-modal>
+      </i-modal>-->
     </div>
   </div>
 </template>
